ae5e1a64b997c0fb3cfefb44c6c97669f3d28657
Dylan-George <dgeorge000@gmail.com>
2016-09-20 16:09:35 -0500

    Add files via upload

--- src/player.js
+++ src/player.js
@@ -13,16 +13,141 @@ module.exports = exports = Player;
  * @param {Postition} position object specifying an x and y
  */
 function Player(position) {
-  this.state = "idle";
-  this.x = position.x;
-  this.y = position.y;
-  this.width  = 64;
-  this.height = 64;
-  this.spritesheet  = new Image();
-  this.spritesheet.src = encodeURI('assets/PlayerSprite2.png');
-  this.timer = 0;
-  this.frame = 0;
+	this.state = "idle";
+	this.x = position.x;
+	this.y = position.y;
+	this.width  = 64;
+	this.height = 64;
+	this.spritesheet  = new Image();
+	this.spritesheet.src = encodeURI('assets/PlayerSprite2.png');
+	this.timer = 0;
+	this.frame = 0;
+
+	
+	this.speed = 4;
+	this.moveTimer = 0;
+	this.moveDelayTime = 150;
+	//Is it finishing a hop
+	this.stillMoving = false;
+	//Is it hopping again
+	this.movingAgain = false;
+	
+	//Which direction is it hopping
+	this.currentDirection = 
+	{
+		up: false,
+		down: false,
+		left: false,
+		right: false
+	}
+	
+	//If hopping again, which direction
+	this.nextDirection = 
+	{
+		up: false,
+		down: false,
+		left: false,
+		right: false
+	}
+	
+	var self = this;
+	window.onkeydown = function(event)
+	{
+		switch(event.keyCode)
+		{
+			//up
+			case 38:
+			case 87:
+				if (!self.stillMoving) 
+				{
+					self.currentDirection.up = true;
+					self.stillMoving = true;
+					self.state = "moving";
+				}
+				self.nextDirection.up = true;
+				self.movingAgain = true;
+				event.preventDefault();
+				break;
+			//down
+			case 40:
+			case 83:
+				if (!self.stillMoving) 
+				{
+					self.currentDirection.down = true;
+					self.stillMoving = true;
+					self.state = "moving";
+				}
+				self.nextDirection.down = true;
+				self.movingAgain = true;
+				event.preventDefault();
+				break;
+			//left
+			case 37:
+			case 65:
+				if (!self.stillMoving) 
+				{
+					self.currentDirection.left = true;
+					self.stillMoving = true;
+					self.state = "moving";
+				}
+				self.nextDirection.left = true;
+				self.movingAgain = true;
+				event.preventDefault();
+				break;
+			//right
+			case 39:
+			case 68:
+				if (!self.stillMoving) 
+				{
+					self.currentDirection.right = true;
+					self.stillMoving = true;
+					self.state = "moving";
+				}
+				self.nextDirection.right = true;
+				self.movingAgain = true;
+				event.preventDefault();
+				break;
+		}
+	}
+	
+	window.onkeyup = function(event)
+	{
+		switch(event.keyCode)
+		{
+			//up
+			case 38:
+			case 87:
+				self.nextDirection.up = false;
+				self.movingAgain = false;
+				event.preventDefault();
+				break;
+			//down
+			case 40:
+			case 83:
+				self.nextDirection.down = false;
+				self.movingAgain = false;
+				event.preventDefault();
+				break;
+			//left
+			case 37:
+			case 65:
+				self.nextDirection.left = false;
+				self.movingAgain = false;
+				event.preventDefault();
+				break;
+			//right
+			case 39:
+			case 68:
+				self.nextDirection.right = false;
+				self.movingAgain = false;
+				event.preventDefault();
+				break;
+		}
+	}
+
 }
+
+
 
 /**
  * @function updates the player object
@@ -31,15 +156,47 @@ function Player(position) {
 Player.prototype.update = function(time) {
   switch(this.state) {
     case "idle":
-      this.timer += time;
+		if(this.movingAgain) this.state = "moving";
+      /*this.timer += time;
       if(this.timer > MS_PER_FRAME) {
         this.timer = 0;
         this.frame += 1;
         if(this.frame > 3) this.frame = 0;
-      }
+      } */
       break;
+	case "moving":
+		this.timer += time;
+		this.moveTimer += time;
+		if(this.timer > MS_PER_FRAME) {
+			this.timer = 0;
+			this.frame += 1;
+			if(this.frame > 3) this.frame = 0;
+		}
+		
+		if(this.moveTimer <= 1000/this.speed)
+		{
+			if(this.currentDirection.up) this.y -= this.speed * this.height * time/1000;
+			else if(this.currentDirection.down) this.y += this.speed * this.height * time/1000;
+			else if(this.currentDirection.right) this.x += this.speed * this.width * time/1000;
+			else if(this.currentDirection.left) this.x -=  this.speed * this.width * time/1000;
+		}
+		else if (this.moveTimer < (1000/this.speed) + this.moveDelayTime);
+		else 
+		{	
+
+			this.stillMoving = false;			
+			this.moveTimer = 0;
+			this.state = "idle";
+			this.currentDirection.up = this.nextDirection.up;
+			this.currentDirection.down = this.nextDirection.down;
+			this.currentDirection.left = this.nextDirection.left;
+			this.currentDirection.right = this.nextDirection.right;
+		}
+ 
+		break;
     // TODO: Implement your player's update by state
   }
+  
 }
 
 /**
@@ -59,6 +216,16 @@ Player.prototype.render = function(time, ctx) {
         this.x, this.y, this.width, this.height
       );
       break;
-    // TODO: Implement your player's redering according to state
+	case "moving":
+		ctx.drawImage(
+        // image
+        this.spritesheet,
+        // source rectangle
+        this.frame * 64, 64, this.width, this.height,
+        // destination rectangle
+        this.x, this.y, this.width, this.height
+      );
+      break;
+    // TODO: Implement your player's rendering according to state
   }
 }
